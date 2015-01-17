ebea44812437f4c3006d5a6f6c32d033a6d423a9
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-01-17 20:30:14 +0100

    fix

--- Dynamic programming/numbersolitary.py
+++ Dynamic programming/numbersolitary.py
@@ -1,6 +1,5 @@
-"""
-In a given array, find the subset of maximal sum in which the distance between consecutive elements is at most 6.
-"""
+# you can use print for debugging purposes, e.g.
+# print "this is a debug message"
 def solution(A):
     n = len(A)
     # array of max M[i] K from square i
@@ -10,13 +9,12 @@ def solution(A):
         for j in xrange(1,7):
             if M[i] == -1 or M[i] < A[j]:
                 M[i] = j
+                print 'j ' + str(j)
             if i + j == n-1:
                 break
         i = i + M[i]
-
     value = A[0]
-    for k in xrange(0, len(M) -1):
+    steps = len(M)
+    for k in xrange(0, steps -1):
         value = value + A[M[k]]
-
-    return value
-
+    return value 
