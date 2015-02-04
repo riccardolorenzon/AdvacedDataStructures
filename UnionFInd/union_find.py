bceb2fa6163f1fd694f44a3fb13b7aa1690f7808
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-02-05 00:12:47 +0100

    quick unionfind weighted trees

--- UnionFInd/union_find.py
+++ UnionFInd/union_find.py
@@ -1,4 +1,4 @@
-class DisjointSet(object):
+class QuickUnionFind(object):
     def __init__(self, n):
         '''
         initialize the array as a singleton array
@@ -27,27 +27,78 @@ class DisjointSet(object):
                 out += ' '
         print out
 
+class QuickUnionWeightedUF:
+    def __init__(self, n):
+        self.id = [0] * n
+        self.sz = [0] * n
+        for i in xrange(0,n):
+            self.id[i] = i
+            self.sz[i] = 1
+
+    def root(self,i):
+        while i != self.id[i]:
+            i = self.id[i]
+        return i
+
+    def connected(self, p, q):
+        return self.root(p) == self.root(q)
+
+    def add(self, p, q):
+        i = self.root(p)
+        j = self.root(q)
+        if i == j:
+            return
+        if self.sz[i] < self.sz[j]:
+            self.id[i] = j
+            self.sz[j] += self.sz[i]
+        else:
+            self.id[j] = i
+            self.sz[i] += self.sz[j]
+        self.show()
+
+    def show(self):
+        out = ""
+        for i in xrange(0, len(self.id)):
+            out += self.id[i].__str__()
+            if i < len(self.id) - 1:
+                out += ' '
+        print out
+    """
+
+    public void show() {
+        String out = "";
+        for( int i=0; i<id.length; i++) {
+            out += id[i];
+            if(i < id.length - 1 ) out += " ";
+        }
+        System.out.println(out);
+    }
+}
+"""
+
+
+
+
+
 if __name__ == '__main__':
-    disjointSet = DisjointSet(10)
-    #0-5 3-0 6-0 9-0 8-4 7-9
-    disjointSet.add(0,5)
-    disjointSet.add(3,0)
+    # 6-8 7-1 2-0 0-9 6-0 3-5
+    disjointSet = QuickUnionFind(10)
+    disjointSet.add(6,8)
+    disjointSet.add(7,1)
+    disjointSet.add(2,0)
+    disjointSet.add(0,9)
     disjointSet.add(6,0)
-    disjointSet.add(9,0)
-    disjointSet.add(8,4)
-    disjointSet.add(7,9)
+    disjointSet.add(3,5)
     disjointSet.show()
-
-    #1-0 6-2 9-8 0-2 8-3 3-4 4-5 7-6 9-2
-
-    disjointSet = DisjointSet(10)
-    disjointSet.add(1,0)
-    disjointSet.add(6,2)
-    disjointSet.add(9,8)
-    disjointSet.add(0,2)
-    disjointSet.add(8,3)
-    disjointSet.add(3,4)
-    disjointSet.add(4,5)
-    disjointSet.add(7,6)
-    disjointSet.add(9,2)
-    disjointSet.show()
+    #4-3 0-2 1-9 6-7 2-1 8-6 4-6 9-6 1-5
+    quickdisjointSet = QuickUnionWeightedUF(10)
+    quickdisjointSet.add(4,3)
+    quickdisjointSet.add(0,2)
+    quickdisjointSet.add(1,9)
+    quickdisjointSet.add(6,7)
+    quickdisjointSet.add(2,1)
+    quickdisjointSet.add(8,6)
+    quickdisjointSet.add(4,6)
+    quickdisjointSet.add(9,6)
+    quickdisjointSet.add(1,5)
+    quickdisjointSet.show()
