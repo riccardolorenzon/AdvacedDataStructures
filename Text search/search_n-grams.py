2238d7d335ee5a3b8a562d5d16dcd07fa13588bb
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-03-15 18:35:53 +0100

    add correct length management

--- Text search/search_n-grams.py
+++ Text search/search_n-grams.py
@@ -8,8 +8,6 @@ def read_lines(input_file, hash_table):
     :param hash_table: hash table containing n-grams occurences
     :return:
     """
-    hash_object = hashlib.md5(b'Hello World')
-    print(hash_object.hexdigest())
     with open(input_file, "r") as in_file:
         current_list, remainder = [], []
         while True:
@@ -18,29 +16,43 @@ def read_lines(input_file, hash_table):
                 break
             current_list += line1.strip().split(' ')
             n = len(current_list)
+            token_type = 0
             for i in xrange(0, n-3):
-                s = ''
-                print current_list[i]
-                if n-3-i > 2:
+                s_2, s_3, s_4 = '','',''
+                if n-i > 2:
                     #check if i + 1 exists
-                    s = ''.join(x for x in current_list[i:4])
-                    print s
-                    pass
-                elif n-3-i > 1:
+                    s_4 = ''.join(x for x in current_list[i:i+4])
+                    token_type = 4
+                    print '4gram ' + s_2
+                    hash_key = hashlib.md5(s_4).hexdigest()
+                    if hash_key in hash_table:
+                        hash_table[hash_key][2] += 1
+                    else:
+                        hash_table[hash_key] = [token_type, s_4, 0]
+                if n-i > 1:
                     #check if i + 2 exists
-                    s = ''.join(x for x in current_list[i:3])
-                    print s
-                    pass
-                elif n-3-i > 0:
+                    s_3 = ''.join(x for x in current_list[i:i+3])
+                    token_type = 3
+                    print '3gram ' + s_2
+                    hash_key = hashlib.md5(s_3).hexdigest()
+                    if hash_key in hash_table:
+                        hash_table[hash_key][2] += 1
+                    else:
+                        hash_table[hash_key] = [token_type, s_3, 0]
+                if n-i > 0:
                     #check if i + 3 exists
-                    s = ''.join(x for x in current_list[i:2])
-                    print s
-                    pass
-                else:
-                    pass
+                    s_2 = ''.join(x for x in current_list[i:i+2])
+                    token_type = 2
+                    print '2gram ' + s_2
+                    hash_key = hashlib.md5(s_2).hexdigest()
+                    if hash_key in hash_table:
+                        hash_table[hash_key][2] += 1
+                    else:
+                        hash_table[hash_key] = [token_type, s_2, 0]
             current_list = current_list[-3:]
         in_file.close()
 
 if __name__ == '__main__':
     hash_table = dict()
     read_lines('./story.txt', hash_table)
+    print hash_table
