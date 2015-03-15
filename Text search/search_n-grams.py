ed85661de99d89fe9d7ab265b0754a21bfdeb794
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-03-15 20:00:05 +0100

    using readlines instead of read

--- Text search/search_n-grams.py
+++ Text search/search_n-grams.py
@@ -18,13 +18,13 @@ def read_lines(input_file, hash_table):
     """
     with open(input_file, "r") as in_file:
         current_list, remainder = [], []
-        while True:
-            line1 = in_file.readline()
-            if line1 == '':
-                break
+        lines = in_file.readlines()
+        for line_number, line1 in enumerate(lines):
             current_list += line1.strip().split(' ')
             n = len(current_list)
-            for i in xrange(0, n-3):
+            for i in xrange(0, n):
+                if n-i < 4 and line_number != len(lines) - 1:
+                    continue
                 s_2, s_3, s_4 = '','',''
                 if n-i > 3:
                     #check if i + 1 exists
@@ -42,18 +42,6 @@ def read_lines(input_file, hash_table):
                     print '2gram ' + s_2
                     upsert_hash_key(2, s_2, hash_table)
             current_list = current_list[-3:]
-        n = len(current_list)
-        for i in xrange(0,n):
-            if n-i > 2:
-                #check if i + 2 exists
-                s_3 = ''.join(x for x in current_list[i:i+3])
-                print '3gram ' + s_3
-                upsert_hash_key(3, s_3, hash_table)
-            if n-i > 1:
-                #check if i + 3 exists
-                s_2 = ''.join(x for x in current_list[i:i+2])
-                print '2gram ' + s_2
-                upsert_hash_key(2, s_2, hash_table)
         in_file.close()
 
 if __name__ == '__main__':
