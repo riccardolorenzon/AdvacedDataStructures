63321a65db110e5bf5ca004f36f404ed071ea1b5
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-03-15 14:46:57 +0100

    add text search

---
+++ Text search/search_n-grams.py
@@ -0,0 +1,10 @@
+__author__ = 'riccardo'
+"""
+Build a tool which receives a corpus of text, analyses it and reports the top 10 most frequent bigrams,
+
+trigrams, four-grams (i.e. most frequently occurring two, three and four word consecutive combinations).
+
+Extra*: Use a string similarity function to aggregate the “similar” bigrams together: Ex: “the-house” and
+
+“tree-house” will be counted as the same bigram.
+"""
