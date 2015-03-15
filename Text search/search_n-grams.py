adb94b039c3d0171f390f0aa394942ae4b6162ce
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-03-15 19:40:34 +0100

    fixed last n < 3 items

--- Text search/search_n-grams.py
+++ Text search/search_n-grams.py
@@ -24,7 +24,7 @@ def read_lines(input_file, hash_table):
                 break
             current_list += line1.strip().split(' ')
             n = len(current_list)
-            for i in xrange(0, n):
+            for i in xrange(0, n-3):
                 s_2, s_3, s_4 = '','',''
                 if n-i > 3:
                     #check if i + 1 exists
@@ -42,6 +42,18 @@ def read_lines(input_file, hash_table):
                     print '2gram ' + s_2
                     upsert_hash_key(2, s_2, hash_table)
             current_list = current_list[-3:]
+        n = len(current_list)
+        for i in xrange(0,n):
+            if n-i > 2:
+                #check if i + 2 exists
+                s_3 = ''.join(x for x in current_list[i:i+3])
+                print '3gram ' + s_3
+                upsert_hash_key(3, s_3, hash_table)
+            if n-i > 1:
+                #check if i + 3 exists
+                s_2 = ''.join(x for x in current_list[i:i+2])
+                print '2gram ' + s_2
+                upsert_hash_key(2, s_2, hash_table)
         in_file.close()
 
 if __name__ == '__main__':
