ea3c6e485b3b8446d4998e0c875d31c32f3542d5
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-03-16 22:42:02 +0100

    unit tests

--- Text search/unittests.py
+++ Text search/unittests.py
@@ -6,22 +6,41 @@ class TestReadLinesAndComputeHashTable(unittest.TestCase):
     contains test methods
     """
     def test_nowords(self):
-        pass
+        lines = ['']
+        hash_table = dict()
+        read_lines(lines, hash_table)
+        self.assertEqual(len(hash_table.keys()),0)
 
     def test_wordsoncorners(self):
-        pass
+        lines = ['a b c d e', 'a f g h e', 'e a b g d g h h']
+        hash_table = dict()
+        read_lines(lines, hash_table)
+        self.assertEqual(hash_table['e a'], [2,2])
 
     def test_multiple4grams(self):
-        pass
+        lines = ['a b c d e', 'a c g h e', 'e d e a c g h h']
+        hash_table = dict()
+        read_lines(lines, hash_table)
+        self.assertEqual(hash_table['d e a c'], [4,2])
 
     def test_multiple3grams(self):
-        pass
+        lines = ['a b c d e', 'a j k h e', 'e e a j c g h h']
+        hash_table = dict()
+        read_lines(lines, hash_table)
+        self.assertEqual(hash_table['e a j'], [3,2])
 
     def test_multiple2grams(self):
-       pass
+        lines = ['a b c d e', 'b c k h b', 'c e a j c g h h']
+        hash_table = dict()
+        read_lines(lines, hash_table)
+        self.assertEqual(hash_table['b c'], [2,3])
 
     def test_spaceseparators(self):
-        pass
+        lines = ['a b c abc cd c', 'd a b c ab', 'c c ac d']
+        hash_table = dict()
+        read_lines(lines, hash_table)
+        self.assertEqual(hash_table['a b c'], [3,2])
+        self.assertEqual(hash_table['ab c'], [2,1])
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestReadLinesAndComputeHashTable)
 unittest.TextTestRunner(verbosity=2).run(suite)
